86f5261cc09966461e6a679e4a8329c1c1915f92
subasree <subasreebs5@gmail.com>
2022-06-25 13:01:34 +0530

    Another method COMPLETED REPEATSTRING

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,18 +1,24 @@
 const repeatString = function(word, count) {
-let result='';
-if(count>0){
-for(let i=1;i<=count;i++){
-     result= result.concat(word);
+// let result='';
+// if(count>0){
+// for(let i=1;i<=count;i++){
+//      result= result.concat(word);
+// }
+// return result;
+// }
+// else if (count==0){
+//     return "";
+// }
+// else{
+//     return "ERROR";
+// }
+// };
+if(count<0){
+    return "ERROR"
+    }
+return word.repeat(count);
+
 }
-return result;
-}
-else if (count==0){
-    return "";
-}
-else{
-    return "ERROR";
-}
-};
 
 // Do not edit below this line
 module.exports = repeatString;
