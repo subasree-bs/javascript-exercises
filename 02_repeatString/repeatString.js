a5990da69267f5ce09bedb1b33c981a8b8fdc335
subasree <subasreebs5@gmail.com>
2022-06-25 12:11:47 +0530

    COMPLETED REPEATSTRING

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(word, count) {
+let result='';
+if(count>0){
+for(let i=1;i<=count;i++){
+     result= result.concat(word);
+}
+return result;
+}
+else if (count==0){
+    return "";
+}
+else{
+    return "ERROR";
+}
 };
 
 // Do not edit below this line
